f0f9e33e1eacc7310ea05f3fcee4c3c1e1b6f889
leski <leski@DESKTOP-QH7AA1C>
2021-05-27 19:43:43 -0500

    Cambio Trapecio

--- Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/Llamado_Figuras.java
+++ Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/Llamado_Figuras.java
@@ -8,6 +8,7 @@ package com.mycompany.trabajo_en_grupo;
 import java.util.Scanner;
 import com.mycompany.trabajo_en_grupo.Rombo;
 import com.mycompany.trabajo_en_grupo.triangulo;
+import com.mycompany.trabajo_en_grupo.Trapecio;
 
 /**
  *
@@ -20,7 +21,7 @@ public class Llamado_Figuras {
      */
     public static void main(String[] args) {
        // Figuras
-        double D=0,d=0,l=0,b=0,a=0;
+        double D=0,d=0,l=0,b=0,a=0, bmayor=0, bmenor=0;
         Scanner sc=new Scanner (System.in);
         System.out.println("ingrese lado");
         l=sc.nextDouble();
@@ -31,6 +32,7 @@ public class Llamado_Figuras {
         
         triangulo t = new triangulo(b,a,l);
         Rombo r = new Rombo (D,d,l);
+       // Trapecio tra =new Trapecio (l,b,a,bmayor,bmenor);
         
         System.out.println("EL AREA ES "+r.area());
         System.out.println("EL PERIMETRO ES "+r.perimetro());
