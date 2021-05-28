99ed4b755b0571ee2395fcfb4b2449f2624c615d
Pc Usuario <Pc Usuario@DESKTOP-LTH9DTB>
2021-05-27 19:02:15 -0500

    cambio figura triangulo

---
+++ Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/triangulo.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.trabajo_en_grupo;
+
+
+public class triangulo {
+    private double base;
+    private double altura;
+    private double lado;
+    
+    public triangulo(double b,double a, double l){
+        this.base = b;
+        this.altura = a;
+        this.lado = l;
+        
+    }
+    public double area(){
+        return (this.base *this.altura)/2;
+    }
+    public double perimetro(){
+        return this.lado * 4 ;
+    }
+}
