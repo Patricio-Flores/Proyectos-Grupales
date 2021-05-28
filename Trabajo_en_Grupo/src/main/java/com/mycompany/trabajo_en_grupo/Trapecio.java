8f8e6e646ac25264170bea457c2ce55a1a4391cc
leski <leski@DESKTOP-QH7AA1C>
2021-05-28 18:30:29 -0500

    Cambio Trapecio

--- Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/Trapecio.java
+++ Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/Trapecio.java
@@ -1,24 +1,28 @@
-
 package com.mycompany.trabajo_en_grupo;
-import java.util.Scanner;
-import javax.swing.JOptionPane;
-
 
 public class Trapecio {
-    public static double lado, base, altura, bmayor, bmenor;
-    public static void datosTrapecio() {
 
-        altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura de trapecio  "));
-        bmayor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base mayor de trapecio "));
-        bmenor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la base menor de trapecio  "));
-        lado = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el lado de trapecio  "));
+    private double lado;
+    private double base;
+    private double altura;
+    private double bmayor;
+    private double bmenor;
+
+    public Trapecio(double l, double b, double h, double bma, double bme) {
+        this.lado = l;
+        this.base = b;
+        this.altura = h;
+        this.bmayor = bma;
+        this.bmenor = bme;
+
     }
 
-    public static void areatrapecio() {
-        JOptionPane.showMessageDialog(null, "El área de trapecio es :" + ((altura * (bmayor * bmenor)) / 2));
+    public double perimetro() {
+        return this.bmayor + this.bmenor + this.lado + this.lado;
     }
 
-    public static void perimetrotrapecio() {
-        JOptionPane.showMessageDialog(null, "el perímetro del trapecio es :" + (bmayor + bmenor + lado + lado));
+    public double area() {
+        return (this.altura * (this.bmayor * this.bmenor)) / 2;
     }
+
 }
